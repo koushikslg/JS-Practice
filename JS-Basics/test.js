afe874964f36064f969577e9a914e67a283a3a1e
koushikslg <101099318+koushikslg@users.noreply.github.com>
2024-10-03 14:40:38 +0000

    praticed today

--- JS-Basics/test.js
+++ JS-Basics/test.js
@@ -1,1 +1,14 @@
-console.log("Koushik")
+let arr = ["koushik",5,6,true]
+
+let func = function(){
+    console.log("gtesfgvjdsf")
+}
+
+func()
+
+let obj = {
+    test : 1,
+    testing : 2
+}
+
+console.log(obj])
